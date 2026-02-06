02e679dce99c4fc129fa0139bcbab3f91e312ee3
Fatunsin Damilare <fatunsindamilare1@gmail.com>
2026-02-06 20:03:17 +0100

    add /api to frontend api.jsx

--- frontend/src/service/api.jsx
+++ frontend/src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-wjzi.onrender.com",
+  baseURL: "https://ecommerce-wjzi.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
